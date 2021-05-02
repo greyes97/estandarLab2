4d464323fe683aa88dfd3d7cbfe5b67ff9a93898
greyes97 <greyesl1@miumg.edu.gt>
2021-05-01 19:26:06 -0600

    First commit added

---
+++ src/main/java/gt/edu/umg/ingenieria/sistemas/ingenieria_software/lab2/standarCalc/StandarCalcApplication.java
@@ -0,0 +1,13 @@
+package gt.edu.umg.ingenieria.sistemas.ingenieria_software.lab2.standarCalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StandarCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StandarCalcApplication.class, args);
+	}
+
+}
